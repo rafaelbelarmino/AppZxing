f88d87491363fa22aa66ecaae551e4cdef41873d
rafaelbelarmino <rafaelon@live.com>
2016-03-10 19:55:19 -0300

    .

---
+++ AppZxing/app/src/main/java/br/edu/ifpb/appzxing/MainActivity.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.appzxing;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
+        scanIntegrator.initiateScan();
+    }
+}
